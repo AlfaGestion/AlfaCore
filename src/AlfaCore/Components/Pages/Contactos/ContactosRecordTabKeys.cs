ead358b636681f78a2d6baeb319f07ed6bc86cd3
ev <eveantunez2003@gmail.com>
2026-08-11 11:57:24 -0300

    .

--- src/AlfaCore/Components/Pages/Contactos/ContactosRecordTabKeys.cs
+++ src/AlfaCore/Components/Pages/Contactos/ContactosRecordTabKeys.cs
@@ -3,6 +3,7 @@ namespace AlfaCore.Components.Pages;
 public static class ContactosRecordTabKeys
 {
     public const string Informacion = "informacion";
+    public const string Actividad = "actividad";
     public const string Observaciones = "observaciones";
     public const string Cuentas = "cuentas";
 }
